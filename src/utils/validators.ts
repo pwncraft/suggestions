e7adbb121bd45f22b87b6d050535eca28b5f5033
pwnstar <55674383+pwncraft@users.noreply.github.com>
2021-01-15 10:16:14 -0600

    Fix empty suggestions

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,7 +1,8 @@
 function isValidSuggestion(desc, author?, guild?): boolean {
   if (typeof desc !== 'string') return false;
   if (desc.length <= 0) return false;
-
+  if (!desc.trim().length) return false;
+  
   return true
 }
 
